c21aa35d2f8089670d692d665ab976b39169c53f
JakkOfKlubs <qwuest123@gmail.com>
2022-02-26 15:43:29 -0500

    Fix zero rule check.

--- Assets/TapCodeScript.cs
+++ Assets/TapCodeScript.cs
@@ -69,14 +69,17 @@ public class TapCodeScript : MonoBehaviour
         Sel.OnInteractEnded += SelRelease;
 
         var SerialNumber = BombInfo.GetSerialNumber();
+        SerialNumber = "P98XR0";
         var movementIx = SerialNumber[5] - '0';
         if (movementIx == 0)
             for (int i = 0; i < 6; i++)
                 if (SerialNumber[i] - '0' >= 0 && SerialNumber[i] - '0' <= 9)
                     movementIx += SerialNumber[i] - '0';
+        movementIx = (movementIx % 10);
         Debug.LogFormat("[Tap Code #{0}] Serial number digit: {1}", _moduleId, movementIx);
 
         var wordIx = Rnd.Range(0, 100);
+        wordIx = 20;
         _chosenWord = _wordList[wordIx];
 
         X = wordIx % 10;
@@ -100,12 +103,12 @@ public class TapCodeScript : MonoBehaviour
             if (SerialNumber[1] >= '0' && SerialNumber[1] <= '9')
             {
                 Debug.LogFormat("[Tap Code #{0}] Serial Number's first two characters are LETTER NUMBER. Moving left.", _moduleId);
-                X = (X - movementIx + 10) % 10;
+                X = (X + (10 - movementIx)) % 10;
             }
             else
             {
                 Debug.LogFormat("[Tap Code #{0}] Serial Number's first two characters are LETTER LETTER. Moving up.", _moduleId);
-                Y = (Y - movementIx + 10) % 10;
+                Y = (Y + (10 - movementIx)) % 10;
             }
         }
 

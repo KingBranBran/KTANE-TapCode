48c3a75aa42540c9635fbf3f86c9757733bd101a
JakkOfKlubs <qwuest123@gmail.com>
2022-02-26 15:56:27 -0500

    Revert zero check rule.

--- Assets/TapCodeScript.cs
+++ Assets/TapCodeScript.cs
@@ -69,13 +69,17 @@ public class TapCodeScript : MonoBehaviour
         Sel.OnInteractEnded += SelRelease;
 
         var SerialNumber = BombInfo.GetSerialNumber();
-        var movementIx = SerialNumber[5] - '0';
-        if (movementIx == 0)
+        int movementIx = 0;
+        if (SerialNumber.Contains('0'))
+        {
             for (int i = 0; i < 6; i++)
                 if (SerialNumber[i] - '0' >= 0 && SerialNumber[i] - '0' <= 9)
                     movementIx += SerialNumber[i] - '0';
-        movementIx = (movementIx % 10);
-        Debug.LogFormat("[Tap Code #{0}] Serial number digit: {1}", _moduleId, movementIx);
+        }
+        else
+            movementIx = SerialNumber[5] - '0';
+        movementIx = movementIx % 10;
+        Debug.LogFormat("[Tap Code #{0}] Movement index: {1}", _moduleId, movementIx);
 
         var wordIx = Rnd.Range(0, 100);
         _chosenWord = _wordList[wordIx];
@@ -109,7 +113,6 @@ public class TapCodeScript : MonoBehaviour
                 Y = (Y + (10 - movementIx)) % 10;
             }
         }
-
         _solutionWord = _wordList[Y * 10 + X];
 
         var chosenConverted = _convertedWordList[Array.IndexOf(_wordList, _chosenWord)].ToUpperInvariant();
